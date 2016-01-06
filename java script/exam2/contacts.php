9ff42633cf53a6cd842e6d34e144697e64a3319f
IuliiaNaboka <naboka@onebestwayto.com>
2016-01-06 11:18:52 +0100

    Update contacts.php

--- java script/exam2/contacts.php
+++ java script/exam2/contacts.php
@@ -1,9 +1,9 @@
 <?
 if (array_key_exists('messageFF', $_POST)) {
    $to = 'julianaboka@gmail.com';
-   $subject = 'Çàïîëíåíà êîíòàêòíàÿ ôîðìà ñ '.$_SERVER['HTTP_REFERER'];
+   $subject = 'You have received new message '.$_SERVER['HTTP_REFERER'];
    $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
-   $message = "Èìÿ: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nÑîîáùåíèå: ".$_POST['messageFF'];
+   $message = "Name: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nMessage: ".$_POST['messageFF'];
    $headers = 'Content-type: text/plain; charset="utf-8"';
    $headers .= "MIME-Version: 1.0\r\n";
    $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
